66522a8a4d5a531915830057e357fe6b4bd4dbd1
Okan Erdemir <okan.erdemir@demirorenteknoloji.com>
2020-12-23 11:52:10 +0300

    first commit

---
+++ RabbitMQ_Pusher/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Pusher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pusher Running");
+            BasicNews news = new BasicNews
+            {
+                Title = "Meteoroloji'den yoğun kar uyarısı",
+                Category = "TÜRKİYE",
+                Description = "Doğu Karadeniz'in kıyı illerinde bugün gök gürültülü sağanak, yüksek kesimlerinde yoğun kar yağışı bekleniyor",
+                Slug = "meteorolojiden-yogun-kar-uyarisi-2294103",
+                Url = "/meteorolojiden-yogun-kar-uyarisi-2294103",
+                Id = 2294103
+            };
+
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: $"news",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                string message = JsonConvert.SerializeObject(news);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                             routingKey: $"news",
+                             basicProperties: null,
+                             body: body);
+            }
+
+            Console.WriteLine("İlgili haber gönderildi");
+            Console.ReadLine();
+
+        }
+
+
+
+        class BasicNews
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Url { get; set; }
+            public string Description { get; set; }
+            public string Slug { get; set; }
+            public string Category { get; set; }
+        }
+    }
+}
---
+++ RabbitMQ_Receive/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive running");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "news",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "news",autoAck:true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+
+        class BasicNews
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Url { get; set; }
+            public string Description { get; set; }
+            public string Slug { get; set; }
+            public string Category { get; set; }
+        }
+    }
+}
